26d3a74caa4f5ceac5d6e43efefead6388d31298
Ryan Edge <ryan.c.edge@outlook.com>
2022-02-09 16:52:32 -0500

    refactor: cleanup code

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,4 @@
-import 'package:excuses/ui/routes.dart';
+import 'package:excuses/ui/app.dart';
 import 'package:flutter/material.dart';
-import 'package:routemaster/routemaster.dart';
 
 void main() => runApp(const ExcusesApp());
-
-class ExcusesApp extends StatelessWidget {
-  const ExcusesApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerDelegate: RoutemasterDelegate(routesBuilder: (context) => routes),
-      routeInformationParser: const RoutemasterParser(),
-      title: 'Excuses',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
-  }
-}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:excuses/ui/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:excuses/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ExcusesApp());
+    await tester.pumpWidget(const ExcusesApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
